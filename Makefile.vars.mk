8a91dab6b0c273ae68c065a2fef933d59f3501cb
Fabian Fischer <glrf@users.noreply.github.com>
2022-09-23 09:50:51 +0200

    Add test case without identity provider

--- Makefile.vars.mk
+++ Makefile.vars.mk
@@ -58,3 +58,4 @@ KUBENT_IMAGE    ?= docker.io/projectsyn/kubent:latest
 KUBENT_DOCKER   ?= $(DOCKER_CMD) $(DOCKER_ARGS) $(root_volume) --entrypoint=/app/kubent $(KUBENT_IMAGE)
 
 instance ?= defaults
+test_instances = tests/defaults.yml tests/no-ldap.yml
